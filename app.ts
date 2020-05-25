26b709f899687447369b72f684d7667016db6d03
srmward <srmward@gmail.com>
2020-05-25 16:06:13 -0700

    CORS Support

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import "./config.ts";
 import router from "./router.ts";
 
 const app = new Application();
+app.use(oakCors());
 const APP_PORT = Deno.env.get("APP_PORT");
 const APP_HOST = Deno.env.get("APP_HOST");
 
