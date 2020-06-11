5e5413f3b116a2228332936367cc7ab4a820c8bd
srmward <srmward@gmail.com>
2020-06-10 19:42:04 -0700

    Get Some Logs Going

--- recipes/getRecipes.ts
+++ recipes/getRecipes.ts
@@ -4,15 +4,18 @@ const airTableUrl = Deno.env.get("AIRTABLE_API_URL") || "";
 const airTableApiKey = Deno.env.get("AIRTABLE_API_KEY") || "";
 
 const getRecipes = async () => {
+  console.log('Fetching recipes ...')
   try {
     const recipes = await (await fetch(`${airTableUrl}/recipeNames`, {
       headers: {
         "Authorization": `Bearer ${airTableApiKey}`,
       },
     })).json();
+    console.log('Recipes found. Data: ')
+    console.log(JSON.stringify(recipes,null,1))
     return recipes;
   } catch (e) {
-    return "Error fetching recipes";
+    console.error('Error fetching recipes');
   }
 };
 export default async ({ response }: { response: Response }) => {
